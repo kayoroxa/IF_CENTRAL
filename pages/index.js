a4d97631db8da656a8b66100b3c88bde2a158b28
Caio Rocha <54248474+kayoroxa@users.noreply.github.com>
2021-05-25 16:55:33 -0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function IndexPage() {
+  return (
+    <div>
+      Hello World.{" "}
+      <Link href="/sabosut">
+        <a>About</a>
+      </Link>
+    </div>
+  )
+}
